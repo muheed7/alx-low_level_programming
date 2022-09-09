e06a077c5b3fb16c17bbc802086cfaa0bab5e3b8
muheed7 <yahaya4502006@gmail.com>
2022-09-08 18:15:04 -0700

    Adding executable file and c files

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - Prints the size of various types based on the computer it is compiled and run on...
+ *
+ * Return: Always 0
+ */
+{
+	printf("Size of char: %zu byte(s)\n", sizeof(char));
+	printf("Size of char: %zu byte(s)\n", sizeof(int));
+	printf("Size of char: %zu byte(s)\n", sizeof(long int));
+	printf("Size of char: %zu byte(s)\n", sizeof(long long int));
+	printf("Size of char: %zu byte(s)\n", sizeof(float));	
+	return (0);
+}
